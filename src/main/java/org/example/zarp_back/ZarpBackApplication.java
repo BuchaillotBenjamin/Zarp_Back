2b31247f89e5f5428b0fd94c3f5540cc665f8e32
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-07-17 19:27:17 -0300

    commit inicial

---
+++ src/main/java/org/example/zarp_back/ZarpBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.zarp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarpBackApplication.class, args);
+    }
+
+}
