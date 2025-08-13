5c6ca63f3939546c00751ad69d2480b7c76c4cb5
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-08-13 18:06:20 -0300

    PATCH a los cors

--- src/main/java/org/example/zarp_back/config/webConfig/WebConfig.java
+++ src/main/java/org/example/zarp_back/config/webConfig/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite todos los endpoints
                         .allowedOrigins("http://localhost:5173") // Solo tu frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Si usás cookies/sesiones
             }
