d04ca6ef870cb7d4b6e4fa4e729120b055c47b95
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-09-24 12:47:28 -0300

    ARREGLO GUARDADO RESENIAS

--- src/main/java/org/example/zarp_back/repository/ReseniaRepository.java
+++ src/main/java/org/example/zarp_back/repository/ReseniaRepository.java
@@ -4,10 +4,16 @@ import org.example.zarp_back.model.entity.Resenia;
 import org.example.zarp_back.model.interfaces.GenericoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReseniaRepository extends GenericoRepository<Resenia, Long> {
 
 
     boolean existsByPropiedadIdAndUsuarioId(Long propiedadId, Long usuarioId);
+
+    Optional<Resenia> findByPropiedadIdAndUsuarioId(Long propiedadId, Long usuarioId);
+
+
 
 }
