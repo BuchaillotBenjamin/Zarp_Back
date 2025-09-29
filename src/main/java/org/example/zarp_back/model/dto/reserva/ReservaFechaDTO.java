72ec8c1b38268dec2903eb34667d2a95cf743051
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-09-29 20:23:17 -0300

    arreglo metodo fechas

--- src/main/java/org/example/zarp_back/model/dto/reserva/ReservaFechaDTO.java
+++ src/main/java/org/example/zarp_back/model/dto/reserva/ReservaFechaDTO.java
@@ -1,7 +1,15 @@
 package org.example.zarp_back.model.dto.reserva;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservaFechaDTO {
 
     private LocalDate fechaInicio;
