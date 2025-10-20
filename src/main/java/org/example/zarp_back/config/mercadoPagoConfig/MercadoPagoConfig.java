dc8842ac9108f52ddd75b8da4c71efb924031b66
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-10-20 10:51:33 -0300

    cambio mpconfig

--- src/main/java/org/example/zarp_back/config/mercadoPagoConfig/MercadoPagoConfig.java
+++ src/main/java/org/example/zarp_back/config/mercadoPagoConfig/MercadoPagoConfig.java
@@ -1,15 +1,30 @@
 package org.example.zarp_back.config.mercadoPagoConfig;
 
 import com.mercadopago.client.oauth.OauthClient;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
 @Configuration
 public class MercadoPagoConfig {
 
-    @Bean
-    public OauthClient oauthClient() {
-        return new OauthClient();
-    }
+    @Value("${mercadopago.access_token}")
+    private String mpAccess;
+    @Value("${mercadopago.back_url.success}")
+    private String mpSuccessBackUrl;
+    @Value("${mercadopago.back_url.pending}")
+    private String mpPendingBackUrl;
+    @Value("${mercadopago.back_url.failure}")
+    private String mpFailureBackUrl;
+    @Value("${api.url}")
+    private String publicUrl;
+    @Value("${mercadopago.client_id}")
+    private String mpClientId;
+    @Value("${mercadopago.client_secret}")
+    private String mpClientSecret;
+    @Value("${mercadopago.secret_key_webhook}")
+    private String mpSecretKeyWebhook;
 
 }
