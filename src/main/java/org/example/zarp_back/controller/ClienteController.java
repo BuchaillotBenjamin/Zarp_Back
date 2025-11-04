56962d4387376d44ad2b27d13200931754ff8e12
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-11-04 17:40:50 -0300

    Update ClienteController.java

--- src/main/java/org/example/zarp_back/controller/ClienteController.java
+++ src/main/java/org/example/zarp_back/controller/ClienteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ClienteController extends GenericoControllerImpl<Cliente, ClienteDTO, ClienteResponseDTO, Long, ClienteService> {
 
     @Autowired
